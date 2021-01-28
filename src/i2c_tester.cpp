ea344da9719f2f38e0d0c00dfc231f1947c8443a
MichaelJNielsen <mjn@es.aau.dk>
2021-01-28 09:07:14 +0100

    first push of c++ edition

---
+++ src/i2c_tester.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    int i = 0;
+    while (true)
+    {
+        i++;
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
